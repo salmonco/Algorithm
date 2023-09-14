e83da519928f3ba00217dc12a3baacc3996cb32f
salmonco <xkqjsslsek80@naver.com>
2023-09-14 16:45:40 +0900

    Add: solve BOJ_20438

---
+++ BOJ/Prefix Sum/20438_출석체크.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => item.split(" ").map(Number));
+const [N, K, Q, M] = input[0];
+const sleep = input[1];
+const send = input[2].filter((item) => !sleep.includes(item));
+
+let sum = [0, 0, 0];
+for (let i = 3; i <= N + 2; i++) {
+  sum[i] = sum[i - 1] + 1;
+
+  if (sleep.includes(i)) continue;
+
+  for (let j = 0; j < send.length; j++) {
+    if (i % send[j] === 0) {
+      // 출석함
+      sum[i]--;
+      break;
+    }
+  }
+}
+// console.log(sum)
+let answer = "";
+for (let i = 0; i < M; i++) {
+  const [S, E] = input[3 + i];
+  // console.log(sum[E] - sum[S - 1]);
+  answer += sum[E] - sum[S - 1] + "\n";
+}
+console.log(answer.substring(0, answer.length - 1));
