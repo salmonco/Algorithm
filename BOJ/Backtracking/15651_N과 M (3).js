691b3abd23885c7ae6359a544a12b08ff410c8f5
salmonco <xkqjsslsek80@naver.com>
2024-08-21 17:41:02 +0900

    [Backtracking] BOJ_15651

---
+++ BOJ/Backtracking/15651_N과 M (3).js
@@ -0,0 +1,30 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map(Number));
+const [N, M] = input[0];
+const ans = [];
+
+const dfs = (idx, cnt, arr) => {
+  if (cnt === M) {
+    ans.push(arr.join(" "));
+    return;
+  }
+  for (let i = 1; i <= N; i++) {
+    dfs(i, cnt + 1, [...arr, i]);
+  }
+};
+
+for (let i = 1; i <= N; i++) {
+  dfs(i, 1, [i]);
+}
+console.log(ans.join("\n"));
+
+/*
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+- 1부터 N까지 자연수 중에서 M개를 고른 수열
+- 같은 수를 여러 번 골라도 된다.
+
+*/
