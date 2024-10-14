fda34a29ab05ff05db37f1fda8007ff665462d23
salmonco <xkqjsslsek80@naver.com>
2024-10-14 11:27:15 +0900

    [DP] BOJ_10942

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
@@ -2,19 +2,22 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n").map((v) => v.split(' ').map(Number))
-const [N] = input[0]
-const seq = input[1]
-seq.unshift(0)
-const [M] = input[2]
-const dp = Array.from({ length: N+1 }, () => Array.from({ length: N+1 }, () => 0))
-for (let i = 1; i <= N-1; i++) {
-    dp[i][i] = 1
-    if (seq[i] === seq[i+1]) {
-        dp[i][i+1] = 1
-    }
+  .split("\n")
+  .map((v) => v.split(" ").map(Number));
+const [N] = input[0];
+const seq = input[1];
+seq.unshift(0);
+const [M] = input[2];
+const dp = Array.from({ length: N + 1 }, () =>
+  Array.from({ length: N + 1 }, () => 0)
+);
+for (let i = 1; i <= N - 1; i++) {
+  dp[i][i] = 1;
+  if (seq[i] === seq[i + 1]) {
+    dp[i][i + 1] = 1;
+  }
 }
-dp[N][N] = 1
+dp[N][N] = 1;
 
 // for (let i = 1; i < N; i++) {
 //     for (let j = i+2; j <= N; j++) {
@@ -24,21 +27,21 @@ dp[N][N] = 1
 //     }
 // }
 for (let diff = 2; diff <= N; diff++) {
-    for (let startIndex = 1; startIndex + diff <= N; startIndex++) {
-        let endIndex = startIndex + diff;
-        if (dp[startIndex + 1][endIndex - 1] && seq[startIndex] === seq[endIndex]) {
-            dp[startIndex][endIndex] = 1;
-        }
+  for (let startIndex = 1; startIndex + diff <= N; startIndex++) {
+    let endIndex = startIndex + diff;
+    if (dp[startIndex + 1][endIndex - 1] && seq[startIndex] === seq[endIndex]) {
+      dp[startIndex][endIndex] = 1;
     }
+  }
 }
 
 // console.log(dp.map((v) => v.join(' ')).join('\n'))
-const ans = []
+const ans = [];
 for (let i = 0; i < M; i++) {
-    const [S, E] = input[3+i]
-    ans.push(dp[S][E])
+  const [S, E] = input[3 + i];
+  ans.push(dp[S][E]);
 }
-console.log(ans.join('\n'))
+console.log(ans.join("\n"));
 
 /*
 10:12
@@ -60,4 +63,14 @@ seq은 인덱스 0부터 시작해서 실수하기 쉬움 -> seq 앞에 unshift�
 dp 쓰면 범위 내부 스캔 한 번만 하면 되므로 n^2이 되는 듯.
 
 4% 실패. 왜? 다른사람풀이랑 이중for문만 다른데 차피 로직 똑같이 않나?
+
+내 이중for문:
+(1, 3~N)
+(2, 4~N)
+--
+다른사람 이중for문:
+(1~N-2, 3~N)
+(1~N-3, 4~N)
+dp를 쓴다면 이렇게 해야 될 듯. 전자의 경우 초기화 이후로 아직 dp값 할당 안 된 것을 참조되는 경우 있음. 예를 들어, (1,4)에서 (2,3)을 참조하는데 아직 (2,3)은 dp값 할당 안 돼서 0을 참조할 거임.
+반면 후자의 경우 범위가 작은 것부터 큰 것까지 dp 할당하기 때문에, 나중에 dp 큰 거에서 내부로 들어가서 범위 작은 거 참조하는 경우에 dp 최적화가 됨.
 */
