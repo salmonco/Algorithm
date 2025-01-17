cf8805ac5f20f9d8032698d8287c1ad74e525c5c
BONG JISU <qhdwltn1380@gmail.com>
2025-01-17 10:20:30 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -5,23 +5,36 @@ const input = require('fs')
   .split('\n')
 const L = Number(input[0])
 const STR = input[1]
-const R = 31
-const MOD = 1234567891
+const R = 31n
+const MOD = 1234567891n
+let exponent = 1n
 
 const getHash = (str) => {
     const arr = str.split('')
     return arr.reduce((acc, cur, i) => {
-        const replacedNum = cur.charCodeAt() - 96
-        const result = replacedNum*(R**i) % MOD
-        return acc + result
-    }, 0)
+        const replacedNum = BigInt(cur.charCodeAt() - 96)
+        const result = replacedNum*exponent % MOD
+        exponent *= R % MOD
+        return (acc + result) % MOD
+    }, 0n)
 }
 
 const ans = getHash(STR)
-console.log(ans)
+console.log(ans.toString())
 
 /*
 9:38
 
 1초. 해시함수 만들기
+
+-> 50점. 부분통과. 개선점이 뭘까. 지수 곱 계산할 때 divide and conquer 해줘야 하나?
+-> 그래도 50점. 흠 뭐지.
+다른사람풀이 보니 매번 반복문에서 exponent를 새로 계산해줄 필요 없이,
+반복문 돌 때마다 변수에 계속 R만큼 곱해주는 식으로 지수 계산하면 하면 시간 절약 가능
+-> 그래도 50점. 뭐지
+exponent에도 MOD로 나눠주기
+반환값인 acc+result 도 MOD로 나눠주기
+-> 그래도 50점. 뭐지
+BigInt 써야 하나? 아직 큰 수 범위 기준에 대해 잘 몰겠음..
+
 */
